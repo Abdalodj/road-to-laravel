96c781e3333d830e12a828636b30b54bae883aec
abdalodj <lodianea98@gmail.com>
2022-03-24 23:10:11 +0000

    vid 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts', function(){
+    return response()->json([
+        'title' => 'mon super titre',
+        'description' => 'ma super description'
+    ]);
+});
+
+Route::get('articles', function () {
+    return view('articles');
+});
