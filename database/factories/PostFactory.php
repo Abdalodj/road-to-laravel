8908f886f2a3164b9e7e8589f862e9f979021db2
abdalodj <lodianea98@gmail.com>
2022-03-26 18:42:20 +0000

    step5: factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'content' => $this->faker->paragraph,
+            'created_at' => now()
+        ];
+    }
+}
--- database/migrations/2022_03_26_140111_create_posts_table.php
+++ database/migrations/2022_03_26_140111_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
+            $table->string('title');
             $table->mediumText('content');
             $table->timestamps();
         });
